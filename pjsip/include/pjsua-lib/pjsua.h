982d6ca6662318d4e457a1f065b8c85ba4138ad5
Jan Brachthäuser <contact@jan-br.de>
2025-05-30 07:01:10 +0200

    Increase MAX_ACC

--- pjsip/include/pjsua-lib/pjsua.h
+++ pjsip/include/pjsua-lib/pjsua.h
@@ -3626,7 +3626,7 @@ PJ_DECL(pj_status_t) pjsua_transport_lis_start( pjsua_transport_id id,
  * Maximum accounts.
  */
 #ifndef PJSUA_MAX_ACC
-#   define PJSUA_MAX_ACC            8
+#   define PJSUA_MAX_ACC            256
 #endif
 
 
